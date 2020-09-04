050ccc148c04c4509eb5feb84fa85a933910a5a6
Josiah Powell <josiahpowell@outlook.com>
2020-09-03 23:22:53 -0500

    Refactored index.js

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const departments = new db.departments();
 
 const intro = () => {
     console.log(`\n\n ____              ____        ____    __       ____        \n/\\  _\`\\    /'\\_/\`\\/\\  _\`\\     /\\  _\`\\ /\\ \\     /\\  _\`\\      \n\\ \\ \\/\\_\\ /\\      \\ \\,\\L\\_\\   \\ \\ \\L\\ \\ \\ \\    \\ \\,\\_\\_\\    \n \\ \\ \\/_/_\\ \\ \\__\\ \\/_\\__ \\    \\ \\ ,__/\\ \\ \\  __\\/_\\__ \\    \n  \\ \\ \\L\\ \\\\ \\ \\_/\\ \\/\\ \\L\\ \\   \\ \\ \\/  \\ \\ \\L\\ \\ /\\ \\_\\ \\  \n   \\ \\____/ \\ \\_\\\\ \\_\\ \`\\____\\   \\ \\_\\   \\ \\____/ \\ \`\\____\\ \n    \\/___/   \\/_/ \\/_/\\/_____/    \\/_/    \\/___/   \\/_____/\n\n\n\n                  BUILT BY: Josiah Powell                  \n\n`);
-    console.log("     This application is currently under construction.\n\n\n");
 }
-
-const getEmployeeSelection = (promptMessage) => selectFromTable(employees, promptMessage, x => x.first_name + " " + x.last_name);
 
 const options = {
     // CREATE
@@ -20,24 +17,24 @@ const options = {
         let last_name = await getInput("Last Name");
         let role = await selectFromTable(roles, "Select a Role?", x => x.title);
         let manager = await getEmployeeSelection("Select a Manager?");
-
+        
         let values = [first_name, last_name, role.id, manager.id];
-
+        
         return employees.insert(values);
     },
     "Create Department": async () => {
         let name = await getInput("Department");
-
+        
         return departments.insert([name]);
     },
     "Create Role": async () => {
         let title = await getInput("Title");
         let salary = await getNumber("Salary");
         let department = await selectFromTable(departments, "Select a Department?", x => x.name);
-
+        
         return roles.insert([title, salary, department.id], () => console.log("Inserted record"));
     },
-
+    
     // READ
     "View all Employees": async () => await employees.get(),
     "View all Departments": async () => await departments.get(),
@@ -46,14 +43,19 @@ const options = {
         let manager = await getEmployeeSelection("Select a Manager?");
         return employees.getByFieldMatch("manager_id", manager.id);
     },
-
+    "View Total Budget by Department": async () => {
+        let selection = await selectFromTable(departments, "Select Department?", x => x.name);
+        
+        return departments.getTotalBudget(selection.id);
+    },
+    
+    // UPDATE
     "Update employee Role": async () => {
         let employee = await getEmployeeSelection("Select Employee?");
         let role = await selectFromTable(roles, "Select Role?", x => x.title);
         
         employees.updateFieldOnId(employee.id, "role_id", role.id);
     },
-
     "Update employee Manager": async () => {
         let employee = await getEmployeeSelection("Select Employee?");
         let manager;
@@ -61,29 +63,28 @@ const options = {
             manager = await getEmployeeSelection("Select Manager?");
             if (manager.id === employee.id) console.log("Manager must be a different selection from employee.");
         } while (manager.id === employee.id)
-
+        
         employees.updateFieldOnId(employee.id, "manager_id", manager.id);
     },
+    
+    // DELETE
     "Delete Employee": async () => {
         let selection = await selectFromTable(employees, "Select Employee?", x => x.first_name + " " + x.last_name);
-
+        
         employees.delete(selection.id);
     },
     "Delete Department": async () => {
         let selection = await selectFromTable(departments, "Select Department?", x => x.name);
-
+        
         departments.delete(selection.id);
     },
     "Delete Role": async () => {
         let selection = await selectFromTable(roles, "Select Role?", x => x.title);
-
+        
         roles.delete(selection.id);
     },
-    "View Total Budget by Department": async () => {
-        let selection = await selectFromTable(departments, "Select Department?", x => x.name);
-
-        return departments.getTotalBudget(selection.id);
-    },
+    
+    // QUIT
     "QUIT": "QUIT",
 };
 
@@ -102,7 +103,7 @@ const run = async (options, recursiveRun = false) => {
                 table(data);
             });
         } catch (error) { }
-
+        
         if (recursiveRun) run(options, recursiveRun);
     });
 }
@@ -124,24 +125,26 @@ const selectFromTable = async (table, promptTitle, getDisplayName) => {
     });
 }
 
+const getEmployeeSelection = (promptMessage) => selectFromTable(employees, promptMessage, x => x.first_name + " " + x.last_name);
+
 const getInput = async (question) => {
     let result;
-
+    
     await prompt({
-        type: 'input',
-        name: 'value',
-        message: question,
-    })
-    .then(res => {
-        result = res.value;
-    });
-
-    return result;
-}
-
-const getNumber = async (question) => {
+            type: 'input',
+            name: 'value',
+            message: question,
+        })
+        .then(res => {
+            result = res.value;
+        });
+        
+        return result;
+    }
+    
+    const getNumber = async (question) => {
     let result;
-
+    
     await prompt({
         type: 'number',
         name: 'value',
@@ -152,7 +155,7 @@ const getNumber = async (question) => {
             console.log("Value must be a number.");
             result = await getNumber(question);
         }
-
+        
         return parseInt(res.value);
     });
 }
@@ -162,7 +165,3 @@ const init = () => {
     run(options, true);
 };
 init();
-
-// REQUIRED...
-// * The URL of the GitHub repository
-// * A video demonstrating the entirety of the app's functionality 
